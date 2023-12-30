3686250fd6b3e4b4e0d31e5f454d7bfd2dbb6ec8
Sinku72499 <136006387+Sinku72499@users.noreply.github.com>
2023-12-30 07:45:35 +0000

    Datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,15 @@
+"use strict"//treat all JS code as newer code
+//alert(3+3)// we are using it in node.js//not in web browser
+let Name="sinku"//string datatype=>""
+let age=22//number datatype//bigint
+let isloggedin=false//boolean datatype=>true /false
+//null=>stand alone value
+//undefined
+//symbol=>uniques
+  
+
+//objects non primitive datatype
+console.log(typeof age);//output=number
+console.log(typeof Name);// output=string
+console.log(typeof null);//output=object
+console.log(typeof undefined);//output=undefined
