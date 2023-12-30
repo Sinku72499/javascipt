b416fa12a088de13052a849d6e623d6aea0ce113
Sinku72499 <136006387+Sinku72499@users.noreply.github.com>
2023-12-30 07:15:12 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const customerId="00001"
+let customerMail="sinku8051@gmail.com"
+var customerName="sinku"
+customerCity="Bihar"
+let customerJob;
+//customerId =2// not changed because it is constant
+customerMail="rahul8234@gmail.com"
+customerName="rahul"
+customerCity="bengaluru"
+/* try not to use var because of issue in block scope and functional scope*/
+console.table([customerId,customerMail,customerName,customerCity,customerJob])
