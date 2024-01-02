284120473ce94f9cb0304bb21512c97725b044c0
Sinku72499 <136006387+Sinku72499@users.noreply.github.com>
2024-01-02 05:02:32 +0000

    conversion operation in dataTypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,22 @@
+let score =undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueis =Number(score);
+// console.log(typeof valueis);
+// console.log(valueis);
+
+// "33"=>33;    "33abc"=>NaN 
+//true=>1; false=>0
+// null=0; undefined=>NaN
+//0=>false;  1=>true
+//""=>false; "sinku"=>true
+//*************************************Operation************************************* */
+let value =4
+let negvalue=-value;
+// console.log (negvalue);
+// console.log(true);//output=>true
+// console.log(+true);//output=>1; console.log(true+);output=>error;
+
+
