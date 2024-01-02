07af0f586fcd9249d7393db8aaf9aefc341b4d09
Sinku72499 <136006387+Sinku72499@users.noreply.github.com>
2024-01-02 13:10:48 +0000

    DatatypeSummery

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,8 @@
+// primitive Datatype: String,Number,Boolean,null,undefined,symbol,BigInt
+// Non- primitive:Array,Object,Function 
+//stack(primitive)*****Heap(Non-primitive) 
+/*STACK(PRIMITIVE):in this we can change the value by taking another variable it will change 
+in another value different from first variable because it gave the copy of first varible*/ 
+/*HEAP(NON-PRIMITIVE): in this we cannot change the value it would change also in first variable 
+we can take first variable and take value from first variable to second variable it changes 
+the value in also the first value because it comes from same refrences*/
