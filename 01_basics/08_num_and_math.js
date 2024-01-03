7c6993de270c7f186d31b276cc4a0e1bf28d4549
Sinku72499 <136006387+Sinku72499@users.noreply.github.com>
2024-01-03 02:22:23 +0000

    num_and_maths

---
+++ 01_basics/08_num_and_math.js
@@ -0,0 +1,25 @@
+const score=100
+//console.log(score)
+
+const balance=new Number(100.8647654)
+//console.log(balance)
+//console.log(balance.toString())//converts into string
+//console.log(balance.toString().length)// gives lenth
+//console.log(balance.toFixed(2))// 2 fixed value after dot
+//console.log(balance.toPrecision(3))// gives precise value
+const newvalue=100000000000
+//console.log(newvalue.toLocaleString('en-IN')) 
+
+//*****************************************Maths********************************************* */
+
+//console.log(Math)
+//console.log(Math.abs(-6))//turn negative value in positive only
+//console.log(Math.round(4.6))// its gives round of value 4.2 is 4
+//console.log(Math.ceil(4.2))// it alway gives uppar value
+//console.log(Math.floor(4.9))// takes lowest value
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1))+min)
