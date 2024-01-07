3b7c9a4d62ac6443865a034e2bdf8e08dc595411
Sinku72499 <136006387+Sinku72499@users.noreply.github.com>
2024-01-07 07:25:48 +0000

    loops

---
+++ 04_iterations/nine.js
@@ -0,0 +1,9 @@
+const mynum=[1,2,3,4]
+// const mytotal=mynum.reduce(function (acc,currval) {
+//     console.log(`acc:${acc} currval:${currval}`)
+//     return acc+currval
+// },0)
+
+
+// const mytotal=mynum.reduce( (acc,currval) => acc+currval,0)
+console.log(mytotal)
---
+++ 04_iterations/six.js
@@ -0,0 +1,25 @@
+// const coding=["js","ruby","python","java"]
+// const value=coding.forEach((item) =>{
+//     //console.log(item)
+//     return item
+// })
+// console.log(value)//output=>undefined
+
+// const mynum=[1,2,3,4,5,6,7,8,9,10]
+// const newnum=mynum.filter( (num) => num > 4 )
+// console.log(newnum)
+
+const mynum=[1,2,3,4,5,6,7,8,9,10]
+// const newnum=mynum.filter( (num) => {
+//     return num > 4
+// } )
+// console.log(newnum)
+//another method output=>[ 5, 6, 7, 8, 9, 10 ]
+// const newnum=[]
+// mynum.forEach((num) => {
+//     if (num>4) {
+//         newnum.push(num)
+        
+//     }
+// })
+// console.log(newnum)
